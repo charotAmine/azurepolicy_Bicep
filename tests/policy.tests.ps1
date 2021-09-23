3ade32bb95f9fbcc7a55fb2133c94d0385a6a6bf
charotAmine <42579419+charotAmine@users.noreply.github.com>
2021-09-23 14:17:43 +0000

    fix script

--- tests/policy.tests.ps1
+++ tests/policy.tests.ps1
@@ -1,7 +1,7 @@
 describe 'Deploy a public storage' {
     it 'Not be deployed' {
         write-output "Starting test : "
-        $output = New-AzDeployment -Location "West Europe" -TemplateFile "$($PSScriptRoot)/bicepFiles/main.bicep" 2>&1
+        $output = New-AzDeployment -Location "West Europe" -TemplateFile "$($PSScriptRoot)/bicepFiles/main.bicep" -errorAction continue 2>&1
         $policyName = "storage-vnet-required-1"
         "$output" -like "*$policyName*" | Should -Be $true
     }
